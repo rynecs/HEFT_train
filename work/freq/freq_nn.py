0b18f5da6b1bf19a7602936aecd4a1be49f4a7ef
Ryne Starnes <greatrhyno2@gmail.com>
2024-10-15 13:48:05 -0400

    Add files via upload

---
+++ work/freq/freq_nn.py
@@ -0,0 +1,109 @@
+import os, sys
+
+# the standard module for tabular data
+import pandas as pd
+
+# the standard module for array manipulation
+import numpy as np
+
+# the standard modules for high-quality plots
+import matplotlib as mp
+import matplotlib.pyplot as plt
+
+# pytorch
+import torch
+import torch.nn as nn
+
+# split data into a training set and a test set
+from sklearn.model_selection import train_test_split
+
+# to reload modules
+import importlib
+
+# some simple dnn untilities
+import dnnutil as dn
+
+%matplotlib inline
+
+# update fonts
+FONTSIZE = 12
+font = {'family' : 'serif',
+        'weight' : 'normal',
+        'size'   : FONTSIZE}
+mp.rc('font', **font)
+
+# set usetex = False if LaTex is not 
+# available on your system or if the 
+# rendering is too slow
+mp.rc('text', usetex=False)
+
+# set a seed to ensure reproducibility
+seed = 42
+rnd  = np.random.RandomState(seed)
+
+
+
+
+datafile = '../work/freqtrain.csv'
+
+df  = pd.read_csv(datafile)
+
+# mean of dihiggs mass
+mean = df['mhh'].mean()
+
+# divide dihiggs masses by twice the mean
+df['mhh'] = df['mhh']/(2*mean)
+df['freq'] = df['freq']/10
+
+# mean of dihiggs mass
+mean = df['mhh'].mean()
+
+# divide dihiggs masses by twice the mean
+df['mhh'] = df['mhh']/(2*mean)
+df['freq'] = df['freq']/10
+
+import hhfreq as NN
+importlib.reload(NN)
+
+name     = NN.name
+model    = NN.model
+features = NN.features
+target   = NN.target
+
+
+modelfile  = '%s.dict' % NN.name
+
+traces = ([], [], [])
+
+traces_step   = 100
+#play w batch size and lr
+n_batch       =  64
+n_iterations  = 500000
+early_stopping=  500
+learning_rate = 1.e-3
+
+av_loss = dn.average_quadratic_loss
+#try quadratic loss
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
+
+traces = dn.train(model, optimizer, 
+                  modelfile, early_stopping,
+                  av_loss,
+                  dn.get_batch, 
+                  train_data, valid_data,
+                  features, target,
+                  n_batch, 
+                  n_iterations,
+                  traces,
+                  step=traces_step)
+
+dn.plot_average_loss(traces)
+
+Load_lowest_loss_model = True
+
+if Load_lowest_loss_model:
+    print('load lowest loss model dictionary: %s' % modelfile)
+    modeldict = torch.load(modelfile)
+    model = NN.model
+    model.load_state_dict(modeldict)
